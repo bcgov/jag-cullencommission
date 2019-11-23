cd56c7d85cc1598de3e8b8479b8b8e5fa041bec5
Wade Barnes <wade.barnes@shaw.ca>
2019-11-23 12:29:06 -0800

    Add promotion pipelines

---
+++ jenkins/config.groovy
@@ -0,0 +1,24 @@
+// Pipeline Configuration Properties
+// Import this file into the pipeline using 'load'.
+class config {
+
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
+
+  // Deployment configuration
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+  public static final String DEV_ENV = "${DEPLOYMENT_ENVIRONMENT_TAGS[0]}"
+  public static final String TEST_ENV = "${DEPLOYMENT_ENVIRONMENT_TAGS[1]}"
+  public static final String PROD_ENV = "${DEPLOYMENT_ENVIRONMENT_TAGS[2]}"
+
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'kyrwwq'
+
+  // Instance Suffix
+  public static final String  SUFFIX = ''
+
+  // Apps - Listed in the order they should be tagged
+  public static final String[] APPS = ['website']
+}
+
+return new config();
