22d797b45c365e817912889fcec10576cca8c25b
Scott Kingdon <scott.kingdon@cullencommission.ca>
2019-12-05 13:39:10 -0800

    Updated the Navbar with latest structure.

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,14 +9,78 @@
                     <p><a href="#">Sub-Link 02</a></p>
                     <p><a href="#">Sub-Link 03</a></p>
                 </div> -->
-                <a href="/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarHome; ?>" id="NavbarHome">Home</div></a>
-                <a href="/introductory-statement/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarIntroState; ?>" id="NavbarIntroState">Introductory Statement</div></a>
-                <a href="/inquiry-team/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarTeam; ?>" id="NavbarTeam">Inquiry Team</div></a>
-                <a href="/participants/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarParticipants; ?>" id="NavbarParticipants">Participants</div></a>
-                <a href="/public-meetings-information/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarMeetings; ?>" id="NavbarMeetings">Public Meetings</div></a>
-                <a href="/about/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarAbout; ?>" id="NavbarAbout">About</div></a>
-                <a href="/contact/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarContact; ?>" id="NavbarContact">Contact</div></a>
-                <a href="/media-inquires/" class="NavbarLink"><div class="NavbarItem <?php echo $navbarMedia; ?>" id="NavbarMedia">Media</div></a>
+                <a href="/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarHome">Home</div>
+                </a>
+                <a class="NavbarLink">
+                    <div class="NavbarItem HasSubNav" id="NavbarComState"><i class="fas fa-chevron-down"></i> Commissioner’s Statements</div>
+                </a>
+                <div class="SubNavList" id="NavbarComStateSubNav">
+                    <p><a href="#">Opening Remarks Public Meetings</a></p>
+                    <p><a href="#">Speaking Notes Oral Hearing</a></p>
+                    <p><a href="#">Introductory Statement</a></p>
+                </div>
+                <a href="/inquiry-team/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarTeam">Inquiry Team</div>
+                </a>
+                <a class="NavbarLink">
+                    <div class="NavbarItem HasSubNav" id="NavbarLegislation"><i class="fas fa-chevron-down"></i> Legislation</div>
+                </a>
+                <div class="SubNavList" id="NavbarLegislationSubNav">
+                    <p><a href="#">Terms of Reference</a></p>
+                    <p><a href="#">Legislation</a></p>
+                </div>
+                <a class="NavbarLink">
+                    <div class="NavbarItem HasSubNav" id="NavbarHearings"><i class="fas fa-chevron-down"></i> Hearings</div>
+                </a>
+                <div class="SubNavList" id="NavbarHearingsSubNav">
+                    <p><a href="#">Schedule</a></p>
+                    <p><a href="#">Witnesses</a></p>
+                    <p><a href="#">Transcripts</a></p>
+                    <p><a href="#">Exhibits</a></p>
+                </div>
+                <a href="#" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarTeam">Rulings</div>
+                </a>
+                <a href="/participants/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarParticipants">Participants</div>
+                </a>
+                <a class="NavbarLink">
+                    <div class="NavbarItem HasSubNav" id="NavbarRepPub"><i class="fas fa-chevron-down"></i> Reports &amp; Publications</div>
+                </a>
+                <div class="SubNavList" id="NavbarRepPubSubNav">
+                    <p><a href="#">Commission Reports</a></p>
+                    <p><a href="#">Practice &amp; Procedures</a></p>
+                    <p><a href="#">Other Reports</a></p>
+                </div>
+                <a class="NavbarLink">
+                    <div class="NavbarItem HasSubNav" id="NavbarWebcast"><i class="fas fa-chevron-down"></i> Webcast</div>
+                </a>
+                <div class="SubNavList" id="NavbarWebcastSubNav">
+                    <p><a href="#">Live Webcast</a></p>
+                    <p><a href="#">Webcast Archives</a></p>
+                </div>
+                <a href="/news-releases/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarTeam">News Releases</div>
+                </a>
+                <a href="#" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarTeam">FAQ</div>
+                </a>
+                <a href="#" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarMeetings">Public Meetings</div>
+                </a>
+                <a href="/contact/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarContact">Contact</div>
+                </a>
+                <!-- <a href="/introductory-statement/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarIntroState">Introductory Statement</div>
+                </a>
+                <a href="/about/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarAbout">About</div>
+                </a>
+                <a href="/media-inquires/" class="NavbarLink">
+                    <div class="NavbarItem" id="NavbarMedia">Media</div>
+                </a> -->
             </div>
         </div>
     </div>
