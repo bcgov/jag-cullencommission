a56e486e3ab5b56fc5ee791420c68ef5aaee69b1
Scott Kingdon <scott.kingdon@cullencommission.ca>
2019-12-05 15:55:00 -0800

    Changed the navbar to fix a few display problems and make it simpler to code.

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,69 +9,43 @@
                     <p><a href="#">Sub-Link 02</a></p>
                     <p><a href="#">Sub-Link 03</a></p>
                 </div> -->
-                <a href="/" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarHome">Home</div>
-                </a>
-                <a class="NavbarLink">
-                    <div class="NavbarItem HasSubNav" id="NavbarComState"><i class="fas fa-chevron-down"></i> Commissioner’s Statements</div>
-                </a>
+                <a href="/" class="NavbarButton">Home</a>
+                <a class="NavbarButton HasSubNav" id="NavbarComState"><i class="fas fa-chevron-down"></i> Commissioner’s Statements</a>
                 <div class="SubNavList" id="NavbarComStateSubNav">
                     <p><a href="#">Opening Remarks Public Meetings</a></p>
                     <p><a href="#">Speaking Notes Oral Hearing</a></p>
                     <p><a href="#">Introductory Statement</a></p>
                 </div>
-                <a href="/inquiry-team/" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarTeam">Inquiry Team</div>
-                </a>
-                <a class="NavbarLink">
-                    <div class="NavbarItem HasSubNav" id="NavbarLegislation"><i class="fas fa-chevron-down"></i> Legislation</div>
-                </a>
+                <a href="/inquiry-team/" class="NavbarButton">Inquiry Team</a>
+                <a class="NavbarButton HasSubNav" id="NavbarLegislation"><i class="fas fa-chevron-down"></i> Legislation</a>
                 <div class="SubNavList" id="NavbarLegislationSubNav">
                     <p><a href="#">Terms of Reference</a></p>
                     <p><a href="#">Legislation</a></p>
                 </div>
-                <a class="NavbarLink">
-                    <div class="NavbarItem HasSubNav" id="NavbarHearings"><i class="fas fa-chevron-down"></i> Hearings</div>
-                </a>
+                <a class="NavbarButton HasSubNav" id="NavbarHearings"><i class="fas fa-chevron-down"></i> Hearings</a>
                 <div class="SubNavList" id="NavbarHearingsSubNav">
                     <p><a href="#">Schedule</a></p>
                     <p><a href="#">Witnesses</a></p>
                     <p><a href="#">Transcripts</a></p>
                     <p><a href="#">Exhibits</a></p>
                 </div>
-                <a href="#" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarTeam">Rulings</div>
-                </a>
-                <a href="/participants/" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarParticipants">Participants</div>
-                </a>
-                <a class="NavbarLink">
-                    <div class="NavbarItem HasSubNav" id="NavbarRepPub"><i class="fas fa-chevron-down"></i> Reports &amp; Publications</div>
-                </a>
+                <a href="#" class="NavbarButton">Rulings</a>
+                <a href="/participants/" class="NavbarButton">Participants</a>
+                <a class="NavbarButton HasSubNav" id="NavbarRepPub"><i class="fas fa-chevron-down"></i> Reports &amp; Publications</a>
                 <div class="SubNavList" id="NavbarRepPubSubNav">
                     <p><a href="#">Commission Reports</a></p>
                     <p><a href="#">Practice &amp; Procedures</a></p>
                     <p><a href="#">Other Reports</a></p>
                 </div>
-                <a class="NavbarLink">
-                    <div class="NavbarItem HasSubNav" id="NavbarWebcast"><i class="fas fa-chevron-down"></i> Webcast</div>
-                </a>
+                <a class="NavbarButton HasSubNav" id="NavbarWebcast"><i class="fas fa-chevron-down"></i> Webcast</a>
                 <div class="SubNavList" id="NavbarWebcastSubNav">
                     <p><a href="#">Live Webcast</a></p>
                     <p><a href="#">Webcast Archives</a></p>
                 </div>
-                <a href="/news-releases/" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarTeam">News Releases</div>
-                </a>
-                <a href="#" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarTeam">FAQ</div>
-                </a>
-                <a href="#" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarMeetings">Public Meetings</div>
-                </a>
-                <a href="/contact/" class="NavbarLink">
-                    <div class="NavbarItem" id="NavbarContact">Contact</div>
-                </a>
+                <a href="/news-releases/" class="NavbarButton">News Releases</a>
+                <a href="#" class="NavbarButton">FAQ</a>
+                <a href="#" class="NavbarButton">Public Meetings</a>
+                <a href="/contact/" class="NavbarButton">Contact</a>
                 <!-- <a href="/introductory-statement/" class="NavbarLink">
                     <div class="NavbarItem" id="NavbarIntroState">Introductory Statement</div>
                 </a>
