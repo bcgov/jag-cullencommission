f7470fa4fa85b25a08c285eb710c644afbb9a2e4
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-01-07 15:49:04 -0800

    Minor refinements for mobile phones, and added a link to go back to the top of the page in the PHP includes.

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,6 @@
     <img src="/images/TwitterLogo.png" class="SocialMediaLogo">
     <div id="Header">
         <div id="SiteHeaderDiv">
-            <h1 class="SiteHeaderMain">Commission of Inquiry into Money Laundering in British Columbia</h1>
+            <h1 class="SiteHeaderMain"><a id="top"></a>Commission of Inquiry into Money Laundering in British Columbia</h1>
         </div>
     </div>
