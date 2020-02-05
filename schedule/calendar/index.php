96cd401cdab2ab66b0d95add2e4c6153d673a9d1
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-02-05 11:57:43 -0800

    Getting the content ready for the new Hearings Schedule page.

--- schedule/calendar/index.php
+++ schedule/calendar/index.php
@@ -12,10 +12,35 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 ?>
+<h1>Hearings Schedule</h1>
 <noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
 <p id="IEMessage">If you are seeing this message then it means that your browser doesn't work with our site. Please
     upgrade your <a href="https://www.google.ca/chrome/">browser for free</a>.</p>
+<h2>Future Hearings</h2>
+<h3>May 25 – June 26:</h3>
+<p>These hearings will deal with an overview of the money laundering topic and regulatory models, as well as attempts to quantify the extent of money laundering activity in British Columbia. These hearings may not occupy the full five-week period in the spring.</p>
+<p>The courtroom for these hearings will be announced in the coming weeks.</p>
+<h3>September 8 – December 22:</h3>
+<p>The main hearings, in which Commission Counsel plan to address specific issues*, include:</p>
+<ul>
+    <li>Gaming, casinos, horseracing</li>
+    <li>Real estate</li>
+    <li>Professional services, including accounting and legal</li>
+    <li>Corporate sector</li>
+    <li>Financial institutions and money services</li>
+    <li>Luxury goods</li>
+    <li>Cryptocurrency</li>
+    <li>Cash-based businesses</li>
+    <li>Trade-based money laundering</li>
+    <li>Other sectors</li>
+    <li>Asset recovery</li>
+    <li>Enforcement and regulation</li>
+    <li>Government response</li>
+    <li>Other jurisdictions’ approaches</li>
+</ul>
+<p>*These topics are not necessarily listed in sequence, and this hearing plan is subject to variation. Information about the dates on which specific topics will be addressed in the hearings will be posted on our website at: <a href="http://www.cullencommission.ca">www.cullencommission.ca</a>.</p>
+<p>The courtroom for these hearings will be announced in the coming weeks.</p>
 <script>
     ! function(f) {
         function e(e) {
