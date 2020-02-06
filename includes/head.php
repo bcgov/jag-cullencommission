601a247e4fa03cea94c14b969c101446881b2f85
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-02-06 14:41:16 -0800

    Added Google Analytics to website.

--- includes/head.php
+++ includes/head.php
@@ -9,17 +9,29 @@ header('X-Frame-Options: DENY');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://kit.fontawesome.com/6bee0296c4.js" crossorigin="anonymous"></script>
-    <script src="/js/jquery.js"></script>
-    <link rel="stylesheet" href="/css/main.css" type="text/css">
-    <title><?php
-    if (isset($title)) {
-        echo $title . " - Cullen Commission";
-    } else {
-        echo "Cullen Commission";
-    }
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157960287-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
 
-    ?></title>
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-157960287-1');
+</script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<script src="https://kit.fontawesome.com/6bee0296c4.js" crossorigin="anonymous"></script>
+<script src="/js/jquery.js"></script>
+<link rel="stylesheet" href="/css/main.css" type="text/css">
+<title><?php
+        if (isset($title)) {
+            echo $title . " - Cullen Commission";
+        } else {
+            echo "Cullen Commission";
+        }
+
+        ?></title>
