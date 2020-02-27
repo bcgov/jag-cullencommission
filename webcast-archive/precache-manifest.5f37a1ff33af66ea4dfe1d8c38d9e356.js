2bbd1466a3b3a39079201ed13f0058e7451c012d
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-02-27 10:38:27 -0800

    Uploading the new Webcast Archive page which now hooks into the Hearings Editor JSON file.

---
+++ webcast-archive/precache-manifest.5f37a1ff33af66ea4dfe1d8c38d9e356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d38e313a2ef3700ac09c06e265276a6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e009ad128e7e19809bf",
+    "url": "./static/js/2.be04e032.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "./static/js/2.be04e032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e9a8ce0ae3dc187b92",
+    "url": "./static/js/main.80b5207f.chunk.js"
+  },
+  {
+    "revision": "638ee21db4fad2e63649",
+    "url": "./static/js/runtime-main.175270eb.js"
+  }
+]);
