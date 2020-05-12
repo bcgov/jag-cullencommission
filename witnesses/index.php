278b2322c5b84feddcdd1ed170decd54bcc9d849
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-05-12 16:11:19 -0700

    Added the witness list to the witness list.

--- witnesses/index.php
+++ witnesses/index.php
@@ -14,6 +14,48 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 <noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
 <p id="IEMessage">If you are seeing this message then it means that your browser doesn't work with our site. Please upgrade your <a href="https://www.google.ca/chrome/">browser for free</a>.</p>
+<table>
+        <tr>
+                <td><p></p></td>
+                <td><p><strong>MONDAY</strong></p></td>
+                <td><p><strong>TUESDAY</strong></p></td>
+                <td><p><strong>WEDNESDAY</strong></p></td>
+                <td><p><strong>THURSDAY</strong></p></td>
+                <td><p><strong>FRIDAY</strong></p></td>
+        </tr>
+        <tr>
+                <td><p><strong>Week 1: May 25-May 29</strong></p></td>
+                <td><p>Stephen Schneider</p></td>
+                <td><p>Stephen Schneider</p></td>
+                <td><p>Stephen Schneider</p></td>
+                <td><p>Simon Lord</p></td>
+                <td><p>Simon Lord</p></td>
+        </tr>
+        <tr>
+                <td><p><strong>Week 2: June 1-June 5</strong></p></td>
+                <td><p>Oliver Bullough</p></td>
+                <td><p>Oliver Bullough</p></td>
+                <td><p>William Gilmore</p></td>
+                <td><p>William Gilmore</p></td>
+                <td><p>Michael Levi, Peter Reuter</p></td>
+        </tr>
+        <tr>
+                <td><p><strong>Week 3: June 8-June 12</strong></p></td>
+                <td><p>Michael Levi, Peter Reuter</p></td>
+                <td><p>Chief Superintendent Gilchrist</p></td>
+                <td><p>Chief Superintendent Gilchrist</p></td>
+                <td><p>Mark Sieben, Chris Dawkins, Megan Harris</p></td>
+                <td><p>Mark Sieben, Chris Dawkins, Megan Harris</p></td>
+        </tr>
+        <tr>
+                <td><p><strong>Week 4: June 15-June 19</strong></p></td>
+                <td><p>Sir Robert Wainwright</p></td>
+                <td><p>Sir Robert Wainwright</p></td>
+                <td><p></p></td>
+                <td><p></p></td>
+                <td><p></p></td>
+        </tr>
+</table>
 <script>!function (i) { function e(e) { for (var r, t, n = e[0], o = e[1], u = e[2], f = 0, l = []; f < n.length; f++)t = n[f], Object.prototype.hasOwnProperty.call(p, t) && p[t] && l.push(p[t][0]), p[t] = 0; for (r in o) Object.prototype.hasOwnProperty.call(o, r) && (i[r] = o[r]); for (s && s(e); l.length;)l.shift()(); return c.push.apply(c, u || []), a() } function a() { for (var e, r = 0; r < c.length; r++) { for (var t = c[r], n = !0, o = 1; o < t.length; o++) { var u = t[o]; 0 !== p[u] && (n = !1) } n && (c.splice(r--, 1), e = f(f.s = t[0])) } return e } var t = {}, p = { 1: 0 }, c = []; function f(e) { if (t[e]) return t[e].exports; var r = t[e] = { i: e, l: !1, exports: {} }; return i[e].call(r.exports, r, r.exports, f), r.l = !0, r.exports } f.m = i, f.c = t, f.d = function (e, r, t) { f.o(e, r) || Object.defineProperty(e, r, { enumerable: !0, get: t }) }, f.r = function (e) { "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) }, f.t = function (r, e) { if (1 & e && (r = f(r)), 8 & e) return r; if (4 & e && "object" == typeof r && r && r.__esModule) return r; var t = Object.create(null); if (f.r(t), Object.defineProperty(t, "default", { enumerable: !0, value: r }), 2 & e && "string" != typeof r) for (var n in r) f.d(t, n, function (e) { return r[e] }.bind(null, n)); return t }, f.n = function (e) { var r = e && e.__esModule ? function () { return e.default } : function () { return e }; return f.d(r, "a", r), r }, f.o = function (e, r) { return Object.prototype.hasOwnProperty.call(e, r) }, f.p = "./"; var r = this.webpackJsonpfrontend = this.webpackJsonpfrontend || [], n = r.push.bind(r); r.push = e, r = r.slice(); for (var o = 0; o < r.length; o++)e(r[o]); var s = n; a() }([])</script>
         <script src="./static/js/2.061d23df.chunk.js"></script>
         <script src="./static/js/main.365fe62f.chunk.js"></script>
