8fa24dff58b0ff348b8d8cc5c47314e5475df8cd
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-05-13 11:22:38 -0700

    News Release going live.

--- schedule/index.php
+++ schedule/index.php
@@ -17,9 +17,11 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 <p id="IEMessage">If you are seeing this message then it means that your browser doesn't work with our site. Please
     upgrade your <a href="https://www.google.ca/chrome/">browser for free</a>.</p>
 <h2>Upcoming Hearings</h2>
-<h3>May 25 – June 26:</h3>
-<p>These hearings will deal with an overview of the money laundering topic and regulatory models, as well as explore the issue of the quantification of money laundering activity in British Columbia. These hearings may not occupy the full five-week period in the spring.</p>
-<p>The courtroom for these hearings will be announced in the coming weeks.</p>
+<h3>May 25 – June 17 (estimated):</h3>
+<p>These hearings will deal with an overview of the money laundering topic and regulatory models. Adjustments to the hearing plan have been made in response to the global pandemic and its impact on some participants and witnesses. At this time, it is expected that these hearings will take approximately three and a half weeks to complete.</p>
+<p>These hearings will be held via videoconferencing.</p>
+<p>A livestream of the hearings will be <a href="http://www.cullencommission.ca/webcast-live/">available here</a>.</p>
+<p>You can see the witness list and their testimony date <a href="http://www.cullencommission.ca/">here</a>.</p>
 <h3>September 8 – December 22:</h3>
 <p>The main hearings, in which Commission Counsel plan to address specific issues*, include:</p>
 <ul>
@@ -38,7 +40,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
     <li>Government response</li>
     <li>Other jurisdictions’ approaches</li>
 </ul>
-<p>*These topics are not necessarily listed in sequence, and this hearing plan is subject to variation. Information about the dates on which specific topics will be addressed in the hearings will be posted on our website at: <a href="http://www.cullencommission.ca">www.cullencommission.ca </a>. </p>
+<p>* These topics are not necessarily listed in sequence, and this hearing plan is subject to variation. Information about the dates on which specific topics will be addressed in the hearings will be posted on our website at: <a href="http://www.cullencommission.ca">www.cullencommission.ca </a>. </p>
 <p>The courtroom for these hearings will be announced in the coming weeks.</p>
 <h2>Completed Hearings</h2>
 <h3>February 24 -26:</h3>
@@ -67,9 +69,83 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
     <li>Canadian Bar Association, BC Branch and Criminal Defence Advocacy Society - <a href="/files/OpeningStatement-CBA-CDAS.pdf" target="_blank">Opening Statement</a></li>
 </ol>
 
-<script>!function (f) { function e(e) { for (var r, t, n = e[0], o = e[1], u = e[2], l = 0, a = []; l < n.length; l++)t = n[l], Object.prototype.hasOwnProperty.call(c, t) && c[t] && a.push(c[t][0]), c[t] = 0; for (r in o) Object.prototype.hasOwnProperty.call(o, r) && (f[r] = o[r]); for (s && s(e); a.length;)a.shift()(); return p.push.apply(p, u || []), i() } function i() { for (var e, r = 0; r < p.length; r++) { for (var t = p[r], n = !0, o = 1; o < t.length; o++) { var u = t[o]; 0 !== c[u] && (n = !1) } n && (p.splice(r--, 1), e = l(l.s = t[0])) } return e } var t = {}, c = { 1: 0 }, p = []; function l(e) { if (t[e]) return t[e].exports; var r = t[e] = { i: e, l: !1, exports: {} }; return f[e].call(r.exports, r, r.exports, l), r.l = !0, r.exports } l.m = f, l.c = t, l.d = function (e, r, t) { l.o(e, r) || Object.defineProperty(e, r, { enumerable: !0, get: t }) }, l.r = function (e) { "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) }, l.t = function (r, e) { if (1 & e && (r = l(r)), 8 & e) return r; if (4 & e && "object" == typeof r && r && r.__esModule) return r; var t = Object.create(null); if (l.r(t), Object.defineProperty(t, "default", { enumerable: !0, value: r }), 2 & e && "string" != typeof r) for (var n in r) l.d(t, n, function (e) { return r[e] }.bind(null, n)); return t }, l.n = function (e) { var r = e && e.__esModule ? function () { return e.default } : function () { return e }; return l.d(r, "a", r), r }, l.o = function (e, r) { return Object.prototype.hasOwnProperty.call(e, r) }, l.p = "./"; var r = this.webpackJsonpcalendar = this.webpackJsonpcalendar || [], n = r.push.bind(r); r.push = e, r = r.slice(); for (var o = 0; o < r.length; o++)e(r[o]); var s = n; i() }([])</script>
-    <script src="./static/js/2.8a915f42.chunk.js"></script>
-    <script src="./static/js/main.5ff3bd3c.chunk.js"></script>
+<script>
+    ! function(f) {
+        function e(e) {
+            for (var r, t, n = e[0], o = e[1], u = e[2], l = 0, a = []; l < n.length; l++) t = n[l], Object.prototype.hasOwnProperty.call(c, t) && c[t] && a.push(c[t][0]), c[t] = 0;
+            for (r in o) Object.prototype.hasOwnProperty.call(o, r) && (f[r] = o[r]);
+            for (s && s(e); a.length;) a.shift()();
+            return p.push.apply(p, u || []), i()
+        }
+
+        function i() {
+            for (var e, r = 0; r < p.length; r++) {
+                for (var t = p[r], n = !0, o = 1; o < t.length; o++) {
+                    var u = t[o];
+                    0 !== c[u] && (n = !1)
+                }
+                n && (p.splice(r--, 1), e = l(l.s = t[0]))
+            }
+            return e
+        }
+        var t = {},
+            c = {
+                1: 0
+            },
+            p = [];
+
+        function l(e) {
+            if (t[e]) return t[e].exports;
+            var r = t[e] = {
+                i: e,
+                l: !1,
+                exports: {}
+            };
+            return f[e].call(r.exports, r, r.exports, l), r.l = !0, r.exports
+        }
+        l.m = f, l.c = t, l.d = function(e, r, t) {
+            l.o(e, r) || Object.defineProperty(e, r, {
+                enumerable: !0,
+                get: t
+            })
+        }, l.r = function(e) {
+            "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+                value: "Module"
+            }), Object.defineProperty(e, "__esModule", {
+                value: !0
+            })
+        }, l.t = function(r, e) {
+            if (1 & e && (r = l(r)), 8 & e) return r;
+            if (4 & e && "object" == typeof r && r && r.__esModule) return r;
+            var t = Object.create(null);
+            if (l.r(t), Object.defineProperty(t, "default", {
+                    enumerable: !0,
+                    value: r
+                }), 2 & e && "string" != typeof r)
+                for (var n in r) l.d(t, n, function(e) {
+                    return r[e]
+                }.bind(null, n));
+            return t
+        }, l.n = function(e) {
+            var r = e && e.__esModule ? function() {
+                return e.default
+            } : function() {
+                return e
+            };
+            return l.d(r, "a", r), r
+        }, l.o = function(e, r) {
+            return Object.prototype.hasOwnProperty.call(e, r)
+        }, l.p = "./";
+        var r = this.webpackJsonpcalendar = this.webpackJsonpcalendar || [],
+            n = r.push.bind(r);
+        r.push = e, r = r.slice();
+        for (var o = 0; o < r.length; o++) e(r[o]);
+        var s = n;
+        i()
+    }([])
+</script>
+<script src="./static/js/2.8a915f42.chunk.js"></script>
+<script src="./static/js/main.5ff3bd3c.chunk.js"></script>
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
 ?>
