dac907104ddac213678145a9cd1ec527c60a205a
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-05-22 14:13:34 -0700

    More testing.

--- webcast-live/index_DEV.php
+++ webcast-live/index_DEV.php
@@ -7,7 +7,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 ?>
 <h1>Live Webcast</h1>
-<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://vimeo.com/event/62773/embed" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://vimeo.com/event/62773/" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
 ?>
