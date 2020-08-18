52bad10cd610f0671962821c6f38e9d199addf8d
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-08-17 19:34:29 -0700

    Testing cache settings.

--- includes/head.php
+++ includes/head.php
@@ -1,11 +1,3 @@
-<?php
-header("Cache-Control: no-cache, no-store, must-revalidate, private"); //HTTP 1.1
-header("Pragma: no-cache"); //HTTP 1.0
-header('Content-Type: text/html; charset=UTF-8');
-header('X-Content-Type-Options: nosniff');
-header("X-XSS-Protection: 1");
-header('X-Frame-Options: DENY');
-?>
 <!DOCTYPE html>
 <html lang="en">
 
