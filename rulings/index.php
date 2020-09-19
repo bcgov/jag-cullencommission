012149b41a3c9e60255ad3cb7357af981c0882c6
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-09-18 17:34:31 -0700

    Added a ruling.

--- rulings/index.php
+++ rulings/index.php
@@ -6,13 +6,14 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 ?>
 <h1>Rulings</h1>
-<P><a href="/files/ApplicationForParticipantStatus-Ruling7.pdf" target="_blank">25 March 2020 - Commission of Inquiry into Money Laundering in British Columbia Application for Standing – Ruling #7 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
-<P><a href="/files/ParticipantsApplicationsBobMackinReleaseApplicationsRuling06.pdf" target="_blank">28 January 2020 - Bob Mackin Request for Release of Applications for Participant Status – Ruling #6 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
-<P><a href="/files/Participant-Application-for-Standing-Ruling-5.pdf" target="_blank">6 January 2020 - Participant Application for Standing – Ruling #5 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
-<P><a href="/files/ParticipantsApplicationsBobMackinRuling04.pdf" target="_blank">8 November 2019 - Application of Bob Mackin for Copies of Application Materials – Ruling #4 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
-<P><a href="/files/ParticipantsApplicationsRuling03.pdf" target="_blank">25 October, 2019 - Participants Rulings – Ruling #3 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
-<P><a href="/files/ParticipantsApplicationsRuling02.pdf" target="_blank">16 October 2019 - Participants Applications – Ruling #2 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
-<P><a href="/files/ParticipantsApplicationsRuling01.pdf" target="_blank">24 September 2019 - Participants Applications – Ruling #1 <i class="fas fa-file-pdf PdfIcon"></i></a></P>
+<p><a href="/files/ApplicationForConfidentialityOrder-Ruling8.pdf" target="_blank">9 September 2020 - Application for Confidentiality Order – Ruling #8 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ApplicationForParticipantStatus-Ruling7.pdf" target="_blank">25 March 2020 - Commission of Inquiry into Money Laundering in British Columbia Application for Standing – Ruling #7 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ParticipantsApplicationsBobMackinReleaseApplicationsRuling06.pdf" target="_blank">28 January 2020 - Bob Mackin Request for Release of Applications for Participant Status – Ruling #6 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/Participant-Application-for-Standing-Ruling-5.pdf" target="_blank">6 January 2020 - Participant Application for Standing – Ruling #5 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ParticipantsApplicationsBobMackinRuling04.pdf" target="_blank">8 November 2019 - Application of Bob Mackin for Copies of Application Materials – Ruling #4 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ParticipantsApplicationsRuling03.pdf" target="_blank">25 October, 2019 - Participants Rulings – Ruling #3 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ParticipantsApplicationsRuling02.pdf" target="_blank">16 October 2019 - Participants Applications – Ruling #2 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ParticipantsApplicationsRuling01.pdf" target="_blank">24 September 2019 - Participants Applications – Ruling #1 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
 ?>
