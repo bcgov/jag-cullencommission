1300beba39f934c4e932159b89b7ea622d3852d7
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-11-02 09:00:08 -0800

    Swapped the Live Stream pages.

--- webcast-live/index_NEW.php
+++ webcast-live/index_NEW.php
@@ -10,7 +10,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 <p style="text-align: center; font-size: 2rem"><strong>Witness Exclusion Order In Effect.</strong></p>
 <p style="text-align: center"><em>If you have been notified that you will be testifying, the Commissioner has directed that, unless you have permission, you may not view hearings, read transcripts or review exhibits for the hearings beginning October 26, 2020, onwards.</em></p>
 <div style="padding:56.25% 0 0 0;position:relative;">
-    <iframe src="https://vimeo.com/event/62773/embed" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    <iframe src="https://www.ustream.tv/embed/23995188?autoplay=true" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
 </div>
 <p style="text-align: center; font-weight: bold">If you are having a problem accessing the main channel for the livestream of the hearings, <a href="backup/">please click here</a>.</p>
 <?php
