a4187d69a771947a394b46e248c5c9291dda3af4
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-11-06 16:07:23 -0800

    Added the test file for the potential new Webcast Live page.

---
+++ auto/index.php
@@ -0,0 +1,22 @@
+<?php
+$title = 'Live Webcast';
+$subNavOpen = '#NavbarWebcast';
+$navWebcastLive = true;
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
+?>
+<h1>Live Webcast</h1>
+<?php
+$time = time();
+$hour = date('G');
+if ($hour >= 14 && $hour <= 17) {
+    echo '<p style="text-align: center; font-size: 2rem"><strong>Witness Exclusion Order In Effect.</strong></p><p style="text-align: center"><em>If you have been notified that you will be testifying, the Commissioner has directed that, unless you have permission, you may not view hearings, read transcripts or review exhibits for the hearings beginning&nbsp;October&nbsp;26,&nbsp;2020,&nbsp;onwards.</em></p><div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://www.ustream.tv/embed/23995188?autoplay=true" frameborder="0" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><p style="text-align: center; font-weight: bold">If you are having a problem accessing the main channel for the livestream of the hearings, <a href="backup/">please click here</a>.</p>';
+} else {
+    echo '<p style="text-align: center"><strong>The hearings have not started yet.<br />They will resume on the next business day Monday-Friday, 9:30am-1:30pm<br />(except on statutory holidays).</strong></p>';
+}
+
+?>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+?>
