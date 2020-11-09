2d965c82d857c29280a277c1b393c80b516f465e
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-11-09 12:01:35 -0800

    Fixed an exhibit link and a ruling link name and created a new Ruling page design.

---
+++ rulings/demo.php
@@ -0,0 +1,73 @@
+<?php
+$title = 'Rulings';
+$navRulings = true;
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
+?>
+<h1>Rulings</h1>
+
+<div class="RulingLinkList">
+    <p><strong>DATE</strong></p>
+    <p><strong>RULING</strong></p>
+    <p><strong>TITLE</strong></p>
+
+    <p>5 November, 2020</p>
+    <p>#14</p>
+    <p><a href="/files/ApplicationForStanding-Ruling14.pdf" target="_blank">Application for Standing</a></p>
+
+    <p>27 October, 2020</p>
+    <p>#13</p>
+    <p><a href="/files/ApplicationForDirectionsRegardingRedactions-Ruling13.pdf" target="_blank">Application for Directions Regarding Redactions</a></p>
+
+    <p>23 October, 2020</p>
+    <p>#12</p>
+    <p><a href="/files/ApplicationForWitnessAccommodation-Ruling12.pdf" target="_blank">Application for Witness Accommodation</a></p>
+
+    <p>16 October, 2020</p>
+    <p>#11</p>
+    <p><a href="/files/ApplicationForStanding-Ruling11.pdf" target="_blank">Application for Standing</a></p>
+    
+    <p>29 September, 2020</p>
+    <p>#10</p>
+    <p><a href="/files/ApplicationForStanding-Ruling10.pdf" target="_blank">Application for Standing</a></p>
+    
+    <p>23 September, 2020</p>
+    <p>#9</p>
+    <p><a href="/files/RenewedApplicationForParticipantStatus-Ruling9.pdf" target="_blank">Renewed Application for Participant Status</a></p>
+    
+    <p>18 September, 2020</p>
+    <p>#8</p>
+    <p><a href="/files/ApplicationForConfidentialityOrder-Ruling8.pdf" target="_blank">Application for Confidentiality Order (Corrigendum issued October 14, 2020)</a></p>
+    
+    <p>25 March, 2020</p>
+    <p>#7</p>
+    <p><a href="/files/ApplicationForParticipantStatus-Ruling7.pdf" target="_blank">Application for Standing</a></p>
+    
+    <p>28 January, 2020</p>
+    <p>#6</p>
+    <p><a href="/files/ParticipantsApplicationsBobMackinReleaseApplicationsRuling06.pdf" target="_blank">Bob Mackin Request for Release of Applications for Participant Status</a></p>
+    
+    <p>6 January, 2020</p>
+    <p>#5</p>
+    <p><a href="/files/Participant-Application-for-Standing-Ruling-5.pdf" target="_blank">Participant Application for Standing</a></p>
+    
+    <p>8 November, 2019</p>
+    <p>#4</p>
+    <p><a href="/files/ParticipantsApplicationsBobMackinRuling04.pdf" target="_blank">Application of Bob Mackin for Copies of Application Materials</a></p>
+    
+    <p>25 October, 2019</p>
+    <p>#3</p>
+    <p><a href="/files/ParticipantsApplicationsRuling03.pdf" target="_blank">Participants Rulings</a></p>
+    
+    <p>16 October, 2019</p>
+    <p>#2</p>
+    <p><a href="/files/ParticipantsApplicationsRuling02.pdf" target="_blank">Participants Applications</a></p>
+    
+    <p>24 September, 2019</p>
+    <p>#1</p>
+    <p><a href="/files/ParticipantsApplicationsRuling01.pdf" target="_blank">Participants Applications</a></p>
+</div>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+?>
--- rulings/index.php
+++ rulings/index.php
@@ -7,11 +7,11 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 ?>
 <h1>Rulings</h1>
 <p><a href="/files/ApplicationForStanding-Ruling14.pdf" target="_blank">5 November 2020 - Application for Standing – Ruling #14 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
-<p><a href="/files/ApplicationForDirectionsRegardingRedactions-Ruling13.pdf" target="_blank">28 October 2020 - Application for Directions Regarding Redactions – Ruling #13 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
-<p><a href="/files/ApplicationForWitnessAccommodation-Ruling12.pdf" target="_blank">25 October 2020 - Application for Witness Accommodation – Ruling #12 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ApplicationForDirectionsRegardingRedactions-Ruling13.pdf" target="_blank">27 October 2020 - Application for Directions Regarding Redactions – Ruling #13 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/ApplicationForWitnessAccommodation-Ruling12.pdf" target="_blank">23 October 2020 - Application for Witness Accommodation – Ruling #12 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
 <p><a href="/files/ApplicationForStanding-Ruling11.pdf" target="_blank">16 October 2020 - Application for Standing – Ruling #11 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
 <p><a href="/files/ApplicationForStanding-Ruling10.pdf" target="_blank">29 September 2020 - Application for Standing – Ruling #10 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
-<p><a href="/files/RenewedApplicationForParticipantStatus-Ruling9.pdf" target="_blank">24 September 2020 - Renewed Application for Participant Status – Ruling #9 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
+<p><a href="/files/RenewedApplicationForParticipantStatus-Ruling9.pdf" target="_blank">23 September 2020 - Renewed Application for Participant Status – Ruling #9 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
 <p><a href="/files/ApplicationForConfidentialityOrder-Ruling8.pdf" target="_blank">18 September 2020 - Application for Confidentiality Order – Ruling #8 (Corrigendum issued October 14, 2020) <i class="fas fa-file-pdf PdfIcon"></i></a></p>
 <p><a href="/files/ApplicationForParticipantStatus-Ruling7.pdf" target="_blank">25 March 2020 - Commission of Inquiry into Money Laundering in British Columbia Application for Standing – Ruling #7 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
 <p><a href="/files/ParticipantsApplicationsBobMackinReleaseApplicationsRuling06.pdf" target="_blank">28 January 2020 - Bob Mackin Request for Release of Applications for Participant Status – Ruling #6 <i class="fas fa-file-pdf PdfIcon"></i></a></p>
