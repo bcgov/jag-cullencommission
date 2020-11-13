ee592b14151bf73eef7d6b749e4191e4448e52f9
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-11-13 11:47:49 -0800

    Proposal for new hearings schedule.

---
+++ schedule/demo.php
@@ -0,0 +1,235 @@
+<?php
+$title = 'Hearings Schedule';
+$subNavOpen = '#NavbarHearings';
+$navSchedule = true;
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php');
+?>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
+?>
+<h1>Hearings Schedule</h1>
+<p>While the Commission has made efforts to organize the hearings thematically, the topic of money laundering does not lend itself to silos and witnesses may address a variety of different topics in their testimony, not limited to the sector in question. As well, witnesses called during later portions of the hearings, may have additional evidence to present on the topic of gaming, casinos, and horse racing.</p>
+<p>Please note: the hearings schedule/witness list is subject to change.</p>
+
+<!--  UPCOMING HEARINGS  -->
+
+<p>For a brief overview of the main hearings, please <a href="/hearings-description/">click here</a>.</p>
+<div class="ScheduleList">
+  <p style="background-color: rgb(204, 204, 204); text-align: center; padding: 5px; grid-column: 1 / span 2;"><strong>UPCOMING HEARINGS: GAMING, CASINOS AND HORSE RACING</strong></p>
+  <p><strong><u>Date</u></strong></p>
+  <p><strong><u>Witness Name</u></strong></p>
+  <p>November 13, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Larry Vander Graaf</strong> (cont'd), Former Gaming Policy and Enforcement Branch (GPEB), Executive Director of Investigations, Former RCMP Officer</li>
+  </ul>
+  <p>November 16, 2020</p>
+  <div class="ScheduleListPanel">
+    <p class="SchedulePanelName">Department of Finance – Canada / Financial Transactions and Reports Analysis Centre of Canada (FINTRAC) Panel:</p>
+    <ul class="ScheduleListWitnesses">
+      <li><strong>Gabriel Ngo</strong>, Senior Advisor, Financial Crimes Policy, Department of Finance – Canada</li>
+      <li><strong>Bruce Wallace</strong>, Manager of Strategic Policy and Reviews, Financial Transactions and Reports Analysis Centre of Canada</li>
+    </ul>
+  </div>
+  <div></div>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Frederica Wilson</strong>, Executive Director and Deputy CEO, Federation of Law Societies of Canada</li>
+  </ul>
+  <p>November 17, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Frederica Wilson</strong> (cont'd), Executive Director and Deputy CEO, Federation of Law Societies of Canada</li>
+    <li><strong>Dr. Katie Benson</strong>, Professor, Lancaster University</li>
+  </ul>
+  <div></div>
+  <div class="AfternoonSession">
+    <p class="AfternoonSessionTitle"><strong>Additional Hearing Starting at 2:30pm</strong></p>
+    <ul class="ScheduleListWitnesses">
+      <li><strong>Fred Pinnock</strong>, Former RCMP Officer and Officer-in-Charge, Integrated Illegal Gaming Enforcement Team</li>
+    </ul>
+  </div>
+  <p>November 18, 2020</p>
+  <div class="ScheduleListPanel">
+    <p class="SchedulePanelName">Law Society of British Columbia Panel:</p>
+    <ul class="ScheduleListWitnesses">
+      <li><strong>Craig Ferris</strong>, QC, President, Law Society of British Columbia</li>
+      <li><strong>Don Avison</strong>, QC, Executive Director / CEO, Law Society of British Columbia</li>
+      <li><strong>Jeanette McPhee</strong>, CFO and Director of Trust Regulation, Law Society of British Columbia</li>
+      <li><strong>Gurprit Bains</strong>, Deputy Chief Legal Officer, Law Society of British Columbia</li>
+    </ul>
+  </div>
+  <p>November 19, 2020</p>
+  <div class="ScheduleListPanel">
+    <p class="SchedulePanelName">Law Society of British Columbia Panel (cont’d):</p>
+    <ul class="ScheduleListWitnesses">
+      <li><strong>Craig Ferris</strong>, QC, President, Law Society of British Columbia</li>
+      <li><strong>Don Avison</strong>, QC, Executive Director / CEO, Law Society of British Columbia</li>
+      <li><strong>Jeanette McPhee</strong>, CFO and Director of Trust Regulation, Law Society of British Columbia</li>
+      <li><strong>Gurprit Bains</strong>, Deputy Chief Legal Officer, Law Society of British Columbia</li>
+    </ul>
+  </div>
+  <p>November 20, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Prof. Michael Levi</strong>, Professor, Cardiff University</li>
+  </ul>
+</div>
+
+<!--  PAST HEARINGS  -->
+
+<p>To view the livestream video archives of past hearings, please see our <a href="/webcast-archive/">Webcast Archive page</a>.</p>
+<p>You can find the transcripts of completed hearings at our <a href="/transcripts/">Transcripts page</a>.</p>
+<p>You can find the exhibits from completed hearings at our <a href="/exhibits/">Exhibits page</a>.</p>
+<div class="ScheduleList">
+  <p style="background-color: rgb(204, 204, 204); text-align: center; padding: 5px; grid-column: 1 / span 2;"><strong>PAST HEARINGS</strong></p>
+  <p><strong><u>Date</u></strong></p>
+  <p><strong><u>Witness Name</u></strong></p>
+  <p>November 12, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Larry Vander Graaf</strong>, Former Gaming Policy and Enforcement Branch (GPEB), Executive Director of Investigations, Former RCMP Officer</li>
+  </ul>
+  <p>November 11, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Remembrance Day</strong>, Hearings Not in Session</li>
+  </ul>
+  <p>November 10, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Daryl Tottenham</strong> (cont’d), British Columbia Lottery Corporation (BCLC) Manager Anti-Money Laundering (AML) Programs, Former New Westminster Police Department Officer</li>
+  </ul>
+  <p>November 9, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Mike Hiller</strong>, Former British Columbia Lottery Corporation (BCLC) Investigator, Former RCMP Officer</li>
+  </ul>
+  <p>November 6, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Fred Pinnock</strong> (cont'd), Former RCMP Officer and Officer-in-Charge, Integrated Illegal Gaming Enforcement Team</li>
+    <li><strong>Tom Robertson</strong>, Former RCMP Officer and Officer-in-Charge, Integrated Illegal Gaming Enforcement Team, Former Gaming Policy and Enforcement Branch (GPEB) Investigator</li>
+  </ul>
+  <p>November 5, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Daryl Tottenham</strong> (cont'd), British Columbia Lottery Corporation (BCLC) Manager Anti-Money Laundering (AML) Programs, Former New Westminster Police Department Officer</li>
+    <li><strong>Fred Pinnock</strong>, Former RCMP Officer and Officer-in-Charge, Integrated Illegal Gaming Enforcement Team</li>
+  </ul>
+  <p>November 4, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Daryl Tottenham</strong>, British Columbia Lottery Corporation (BCLC) Manager Anti-Money Laundering (AML) Programs, Former New Westminster Police Department Officer</li>
+  </ul>
+  <p>November 3, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Rob Barber</strong>, Former Gaming Policy and Enforcement Branch (GPEB) Investigator, Former RCMP Officer</li>
+    <li><strong>Muriel Labine</strong>, Former Great Canadian Gaming Corporation (GCGC) Dealer Supervisor</li>
+  </ul>
+  <p>November 2, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Ken Ackles</strong>, Gaming Policy and Enforcement Branch (GPEB) Manager of Investigations, Former RCMP Officer</li>
+  </ul>
+  <p>October 30, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>John Karlovcec</strong> (cont'd), Former British Columbia Lottery Corporation (BCLC) Director, Anti-Money Laundering (AML) &amp; Investigations, Former RCMP Officer</li>
+  </ul>
+  <p>October 29, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Gord Friesen</strong> (cont'd), Former British Columbia Lottery Corporation (BCLC) Manager of Investigations, Former RCMP Officer</li>
+    <li><strong>John Karlovcec</strong>, Former British Columbia Lottery Corporation (BCLC) Director, Anti-Money Laundering (AML) &amp; Investigations, Former RCMP Officer</li>
+  </ul>
+  <p>October 28, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Ward Clapham</strong> (cont'd), Former Officer-in-Charge, Richmond RCMP Detachment</li>
+    <li><strong>Gord Friesen</strong>, Former British Columbia Lottery Corporation (BCLC) Manager of Investigations, Former RCMP Officer</li>
+  </ul>
+  <p>October 27, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Stone Lee</strong>, British Columbia Lottery Corporation (BCLC) Investigator, Former Great Canadian Gaming Corporation (GCGC) Surveillance Manager</li>
+    <li><strong>Ward Clapham</strong>, Former Officer-in-Charge, Richmond RCMP Detachment</li>
+  </ul>
+  <p>October 26, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Steve Beeksma</strong>, British Columbia Lottery Corporation (BCLC) Anti-Money Laundering (AML) Project Specialist, Former Great Canadian Gaming Corporation (GCGC) Surveillance Shift Manager</li>
+  </ul>
+  <p>June 16, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Sir Robert Wainwright</strong> (cont'd), Former Executive Director of Europol and Partner with Deloitte (Netherlands)</li>
+  </ul>
+  <p>June 15, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Sir Robert Wainwright</strong>, Former Executive Director of Europol and Partner with Deloitte (Netherlands)</li>
+  </ul>
+  <p>June 12, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Mark Sieben</strong> (cont'd), Deputy Solicitor General, BC Ministry of Public Safety and Solicitor General</li>
+    <li><strong>Chris Dawkins</strong> (cont'd), Executive Lead, Finance Real Estate Data Analytics, BC Ministry of Finance</li>
+    <li><strong>Megan Harris</strong> (cont'd), Executive Director and Anti-Money Laundering Secretariat Lead, BC Ministry of Attorney General</li>
+  </ul>
+  <p>June 11, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Mark Sieben</strong>, Deputy Solicitor General, BC Ministry of Public Safety and Solicitor General</li>
+    <li><strong>Chris Dawkins</strong>, Executive Lead, Finance Real Estate Data Analytics, BC Ministry of Finance</li>
+    <li><strong>Megan Harris</strong>, Executive Director and Anti-Money Laundering Secretariat Lead, BC Ministry of Attorney General</li>
+  </ul>
+  <p>June 10, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Robert Gilchrist</strong> (cont'd), RCMP Chief Superintendent, Director General of the Criminal Intelligence Service of Canada</li>
+    <li><strong>Leslie Stevens</strong> (cont'd), Inspector, Criminal Intelligence Service British Columbia/Yukon Territory Bureau</li>
+    <li><strong>Ryland Wellwood</strong> (cont'd), Civilian Member Analyst, RCMP</li>
+  </ul>
+  <p>June 9, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Robert Gilchrist</strong>, RCMP Chief Superintendent, Director General of the Criminal Intelligence Service of Canada</li>
+    <li><strong>Leslie Stevens</strong>, Inspector, Criminal Intelligence Service British Columbia/Yukon Territory Bureau</li>
+    <li><strong>Ryland Wellwood</strong>, Civilian Member Analyst, RCMP</li>
+  </ul>
+  <p>June 8, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Michael Levi</strong> (cont'd), Professor, Cardiff University</li>
+    <li><strong>Peter Reuter</strong> (cont'd), Professor, University of Maryland School of Public Policy and Department of Criminology</li>
+  </ul>
+  <p>June 5, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Michael Levi</strong>, Professor, Cardiff University</li>
+    <li><strong>Peter Reuter</strong>, Professor, University of Maryland School of Public Policy and Department of Criminology</li>
+  </ul>
+  <p>June 4, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>William Gilmore</strong> (cont'd), Emeritus Professor, School of Law, University of Edinburgh, Scotland</li>
+  </ul>
+  <p>June 3, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>William Gilmore</strong>, Emeritus Professor, School of Law, University of Edinburgh, Scotland</li>
+  </ul>
+  <p>June 2, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Oliver Bullough</strong> (cont'd), Journalist and Author</li>
+  </ul>
+  <p>June 1, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Oliver Bullough</strong>, Journalist and Author</li>
+  </ul>
+  <p>May 29, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Simon Lord</strong> (cont'd), Senior Officer and Money Laundering Expert, National Crime Agency (UK)</li>
+  </ul>
+  <p>May 28, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Simon Lord</strong>, Senior Officer and Money Laundering Expert, National Crime Agency (UK)</li>
+  </ul>
+  <p>May 27, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Stephen Schneider</strong> (cont'd), Professor, St. Mary's University, Halifax</li>
+  </ul>
+  <p>May 26, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Stephen Schneider</strong> (cont'd), Professor, St. Mary's University, Halifax</li>
+  </ul>
+  <p>May 25, 2020</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Stephen Schneider</strong>, Professor, St. Mary's University, Halifax</li>
+  </ul>
+  <p>February 26, 2020</p>
+  <ul class="ScheduleListWitnesses"></ul>
+  <p>February 25, 2020</p>
+  <ul class="ScheduleListWitnesses"></ul>
+  <p>February 24, 2020</p>
+  <ul class="ScheduleListWitnesses"></ul>
+</div>
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
+?>
