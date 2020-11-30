3995aa9387aee804ecba889bca0cd60f0a401d0f
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-11-30 14:58:48 -0800

    Added November 27 transcript.

--- schedule/index.php
+++ schedule/index.php
@@ -190,7 +190,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
     </ul>
   </div>
   <div class="ScheduleTranscriptButton">
-    <div></div>
+    <a href="/data/transcripts/Transcript November 27, 2020.pdf" target="_blank" class="ScheduleTranscriptLink"><button class="Button TonedDownButton ButtonMarginLeft ButtonMarginRight">Download Transcript</button></a>
     <button class="Button TonedDownButton ButtonMarginLeft ButtonMarginRight" onclick="openVideo('484665779'); return false">Watch Video of Session</button>
   </div>
   <p>November 26, 2020</p>
