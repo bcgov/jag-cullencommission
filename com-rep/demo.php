04f47fe734b92063ffd247dc4e8902dcb843d1a3
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-12-03 16:31:52 -0800

    Made changes to the way that the Interim Report will be displayed.

--- com-rep/demo.php
+++ com-rep/demo.php
@@ -7,7 +7,6 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 ?>
 <h1>Commission Reports</h1>
-<p>Reports developed by the Commission Into Money Laundering in British Columbia will be uploaded to this section in the coming months.</p>
 <div class="ReportList">
     <p class="ReportListDate">December 3, 2020</p>
     <p class="ReportListTitle"><a href="/files/reports/CullenCommission-InterimReport.pdf" target="_blank">Cullen Commission Interim Report</a></p>
