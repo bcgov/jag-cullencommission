8223cd76141ad0c330dfaaf49f6f8e67f4a7a249
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-12-10 11:12:02 -0800

    Interim Report released.

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/lang/welcome_' . $lang);
         <p class="LatestEventsTitle">Latest Information</p>
         <div class="LatestEventsContainer">
             <div class="LatestEvent">
+                <p class="LatestEventDesc">December 10, 2020<br /><strong>News Release</strong><br /><a href="/media/?open=23">Interim Report Released Publicly</a></p>
+            </div>
+            <div class="LatestEvent">
                 <p class="LatestEventDesc">December 7, 2020<br /><strong>Ruling</strong><br /><a href="/rulings/">Application for Directions Regarding Redactions – Ruling #22</a></p>
             </div>
             <div class="LatestEvent">
                 <p class="LatestEventDesc">December 4, 2020<br /><strong>Ruling</strong><br /><a href="/rulings/">Application for Standing – Ruling #21</a></p>
-            </div>
-            <div class="LatestEvent">
-                <p class="LatestEventDesc">December 2, 2020<br /><strong>Ruling</strong><br /><a href="/rulings/">Application for Witness Accommodation – Ruling #20</a></p>
             </div>
         </div>
     </div>
@@ -37,6 +37,11 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/lang/welcome_' . $lang);
     <a href="/?ln=fas" class="LanguageLink"><button class="Button TonedDownButton SmallerButtonLabel ButtonBarButton LanguageButton">Farsi</button></a>
     <a href="/?ln=kor" class="LanguageLink"><button class="Button TonedDownButton SmallerButtonLabel ButtonBarButton LanguageButton">Korean</button></a>
     <a href="/?ln=fil" class="LanguageLink"><button class="Button TonedDownButton SmallerButtonLabel ButtonBarButton LanguageButton">Filipino</button></a>
+</div>
+<div class="AnnouncementContainer">
+    <p class="AnnouncementHeading">Interim Report Released</p>
+    <p style="font-size: 0.85rem; margin: 0px 0px 30px 0px">December 8</p>
+    <p><a href="/com-rep/">View report here</a></p>
 </div>
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/lang/home_' . $lang);
