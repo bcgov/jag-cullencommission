4eedbaf11214c5f2781013c2aa4fe6e2b9768b57
Scott Kingdon <scott.kingdon@cullencommission.ca>
2020-12-10 17:02:17 -0800

    Added December 9 video.

--- schedule/index.php
+++ schedule/index.php
@@ -110,7 +110,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
   </ul>
   <div class="ScheduleTranscriptButton">
     <div></div>
-    <div></div>
+    <button class="Button TonedDownButton ButtonMarginLeft ButtonMarginRight" onclick="openVideo('489244617'); return false">Watch Video of Session</button>
   </div>
   <p>December 8, 2020</p>
   <div>
