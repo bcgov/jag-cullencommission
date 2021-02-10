3e11d16c1166bed5939ec805d1208326b70f9736
Scott Kingdon <scott.kingdon@cullencommission.ca>
2021-02-10 14:11:21 -0800

    Changed the order of the practice and procedures on the Practice and Procedure page.

--- prac-pro/index.php
+++ prac-pro/index.php
@@ -6,8 +6,8 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
 ?>
 <h1>Practice and Procedure</h1>
-<p><a href="/rules-for-standing/">Rules for Standing</a></p>
 <p><a href="/pub-inq-act/">Public Inquiry Act</a></p>
+<p><a href="/rules-for-standing/">Rules for Standing</a></p>
 <p><a href="/files/Rules-of-Practice-and-Procedure.pdf" target="_blank">Rules of Practice and Procedure December 5th, 2019</a></p>
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
