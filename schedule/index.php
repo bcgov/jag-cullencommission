38d18a5e2ecfd8330de6a4c3c332c93b669ee2f5
Scott Kingdon <scott.kingdon@cullencommission.ca>
2021-02-18 15:53:56 -0800

    Updated hearings schedule.

--- schedule/index.php
+++ schedule/index.php
@@ -17,24 +17,6 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
   <p style="background-color: rgb(204, 204, 204); text-align: center; padding: 5px; grid-column: 1 / span 2;"><strong>UPCOMING HEARINGS</strong></p>
   <p><strong><u>Date</u></strong></p>
   <p><strong><u>Witness Name</u></strong></p>
-  <p>February 18, 2021</p>
-  <div>
-    <ul class="ScheduleListWitnesses">
-      <li><strong>Aled ab Iorwerth</strong>, Deputy Chief Economist, Canada Mortgage and Housing Corporation</li>
-    </ul>
-    <div class="ScheduleListPanel">
-      <p class="SchedulePanelName">Housing Prices Panel:</p>
-      <ul class="ScheduleListWitnesses">
-        <li><strong>Professor Tsur Somerville</strong>, University of British Columbia</li>
-        <li><strong>Professor Joshua Gordon</strong>, Simon Fraser University</li>
-        <li><strong>Professor David Ley</strong>, University of British Columbia</li>
-      </ul>
-    </div>
-  </div>
-  <div class="ScheduleTranscriptButton">
-    <div></div>
-    <div></div>
-  </div>
   <p>February 19, 2021</p>
   <ul class="ScheduleListWitnesses">
     <li><strong>Professor Henry Yu</strong>, University of British Columbia</li>
@@ -114,6 +96,24 @@ NEW HEARING TEMPLATE
   <p style="background-color: rgb(204, 204, 204); text-align: center; padding: 5px; grid-column: 1 / span 2;"><strong>PAST HEARINGS</strong></p>
   <p><strong><u>Date</u></strong></p>
   <p><strong><u>Witness Name</u></strong></p>
+  <p>February 18, 2021</p>
+  <div>
+    <ul class="ScheduleListWitnesses">
+      <li><strong>Aled ab Iorwerth</strong>, Deputy Chief Economist, Canada Mortgage and Housing Corporation</li>
+    </ul>
+    <div class="ScheduleListPanel">
+      <p class="SchedulePanelName">Housing Prices Panel:</p>
+      <ul class="ScheduleListWitnesses">
+        <li><strong>Professor Tsur Somerville</strong>, University of British Columbia</li>
+        <li><strong>Professor Joshua Gordon</strong>, Simon Fraser University</li>
+        <li><strong>Professor David Ley</strong>, University of British Columbia</li>
+      </ul>
+    </div>
+  </div>
+  <div class="ScheduleTranscriptButton">
+    <div></div>
+    <div></div>
+  </div>
   <p>February 17, 2021</p>
   <div>
     <div class="ScheduleListPanel">
