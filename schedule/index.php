9438bf701e4b1c64e26a75809f18048e302fdaae
Scott Kingdon <scott.kingdon@cullencommission.ca>
2021-02-19 13:08:40 -0800

    Updated hearings schedule.

--- schedule/index.php
+++ schedule/index.php
@@ -17,14 +17,6 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
   <p style="background-color: rgb(204, 204, 204); text-align: center; padding: 5px; grid-column: 1 / span 2;"><strong>UPCOMING HEARINGS</strong></p>
   <p><strong><u>Date</u></strong></p>
   <p><strong><u>Witness Name</u></strong></p>
-  <p>February 19, 2021</p>
-  <ul class="ScheduleListWitnesses">
-    <li><strong>Professor Henry Yu</strong>, University of British Columbia</li>
-  </ul>
-  <div class="ScheduleTranscriptButton">
-    <div></div>
-    <div></div>
-  </div>
   <p>February 22, 2021</p>
   <ul class="ScheduleListWitnesses">
     <li><strong>Samantha Gale</strong>, CEO, Canadian Mortgage Brokers Association – British Columbia</li>
@@ -96,6 +88,14 @@ NEW HEARING TEMPLATE
   <p style="background-color: rgb(204, 204, 204); text-align: center; padding: 5px; grid-column: 1 / span 2;"><strong>PAST HEARINGS</strong></p>
   <p><strong><u>Date</u></strong></p>
   <p><strong><u>Witness Name</u></strong></p>
+  <p>February 19, 2021</p>
+  <ul class="ScheduleListWitnesses">
+    <li><strong>Professor Henry Yu</strong>, University of British Columbia</li>
+  </ul>
+  <div class="ScheduleTranscriptButton">
+    <div></div>
+    <div></div>
+  </div>
   <p>February 18, 2021</p>
   <div>
     <ul class="ScheduleListWitnesses">
