b0f5b4594751e776497a1cf63c319dff943b07d5
Scott Kingdon <scott.kingdon@cullencommission.ca>
2021-03-24 13:04:07 -0700

    Removed the test that Stuart did now that it is no longer needed.

--- public-meetings/index.php
+++ public-meetings/index.php
@@ -74,7 +74,6 @@ include($_SERVER['DOCUMENT_ROOT'] . '/includes/navbar.php');
     </div>
 </div>
 <br class="ClearFloats">
-<p style="color: white">TEST</p>
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
 ?>
