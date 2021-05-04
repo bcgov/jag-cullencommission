93ba5ed95a9719037a1042ea6975517384333f03
Scott Kingdon <scott.kingdon@cullencommission.ca>
2021-05-04 09:42:34 -0700

    Added May 3 video.

--- schedule/index.php
+++ schedule/index.php
@@ -139,7 +139,7 @@ NEW HEARING TEMPLATE
   </div>
   <div class="ScheduleTranscriptButton">
     <div></div>
-    <div></div>
+    <button class="Button TonedDownButton ButtonMarginLeft ButtonMarginRight" onclick="openVideo('544854110'); return false">Watch Video of Session</button>
   </div>
   <p>April 30, 2021</p>
   <ul class="ScheduleListWitnesses">
